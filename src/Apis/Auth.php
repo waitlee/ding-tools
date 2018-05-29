809b68cd4647a153e061737857e0c1ada80fbfc5
waitlee <liduabc@163.com>
2018-05-29 17:42:44 +0800

    update auth

--- src/Apis/Auth.php
+++ src/Apis/Auth.php
@@ -11,6 +11,9 @@ class Auth
     /**
      * get Access Token
      *
+     * @param string $corpId
+     * @param string $corpSecret
+     *
      * @return string json format
      */
     public static function getAccessToken($corpId, $corpSecret)
@@ -19,6 +22,56 @@ class Auth
 
         return $response->getBody()->getContents();
     }
+
+    /**
+     * 获取用户授权的持久授权码
+     *
+     * @param string $code
+     * @param string $accessToken
+     *
+     * @return mixed
+     */
+    public function getPersistentCode($code, $accessToken)
+    {
+        $response = DingClient::getInstance()
+            ->post('/get_persistent_code', ['access_token' => $accessToken], ['tmp_auth_code' => $code]);
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * 获取用户授权的SNS_TOKEN
+     *
+     * @param string $code
+     * @param string $openId
+     * @param string $accessToken
+     *
+     * @return mixed
+     */
+    public function getSnsToken($code, $openId, $accessToken)
+    {
+        $response = DingClient::getInstance()
+            ->post('/get_sns_token',
+                ['access_token' => $accessToken],
+                ['openid' => $openId, 'persistent_code' => $code]
+            );
+
+        return $response->getBody()->getContents();
+    }
+
+    /**
+     * 获取用户授权的个人信息
+     *
+     * @param string $snsToken
+     */
+    public function getSnsInfo($snsToken)
+    {
+        $response = DingClient::getInstance()
+            ->post('/getuserinfo', ['sns_token' => $snsToken]);
+
+        return $response->getBody()->getContents();
+    }
+
 
     public static function getTicket($accessToken)
     {
